2cf90eaea0c50a8efa635961887118635a685a9d
AGENTICS.MD <adhinton5298@gmail.com>
2026-08-12 21:59:25 -0400

    Remove deprecated opengrants package shim

--- src/opengrants/sources/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Additional funding sources beyond Grants.gov (NSF, open-source funds, donations/gifting)."""
-
-from .public_goods import PublicGoodsFundingClient
-from .nsf import NSFClient
-
-__all__ = ["PublicGoodsFundingClient", "NSFClient"]
